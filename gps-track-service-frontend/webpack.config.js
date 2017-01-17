519bb301a76ca48f4d22e2b47e9eb9b578da64fe
Cornelius Wichering <cornelius.lilge@gmail.com>
2017-01-17 22:52:43 +0100

    Added basic typescript support

--- gps-track-service-frontend/webpack.config.js
+++ gps-track-service-frontend/webpack.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-var, strict, prefer-arrow-callback */
+'use strict';
+
 var packageJSON = require('./package.json');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
@@ -8,14 +11,36 @@ const PATHS = {
 };
 
 module.exports = {
-  entry: './app/index.js',
-
+  entry: {
+    main: './app/index.ts',
+    vendor: [
+      'babel-polyfill',
+      'events'
+    ]
+  },
   output: {
     path: PATHS.build,
     publicPath: '/',
     filename: 'app-bundle.js'
   },
-  
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  module: {
+     loaders: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?presets[]=es2016&presets[]=es2015!ts-loader'
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2016', 'es2015']
+      }
+    }]
+  },
+
   devServer: {
     proxy: {
       '/api': {
@@ -25,5 +50,8 @@ module.exports = {
     }
   },
 
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin(),
+    require('webpack-fail-plugin')
+  ]
 };
