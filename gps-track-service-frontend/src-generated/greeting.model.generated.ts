9688630cee95302c645398deffc31f414f48777a
Cornelius Wichering <cornelius.wichering@engram.de>
2017-01-23 14:03:40 +0100

    Generierten Code entfernt

--- gps-track-service-frontend/src-generated/greeting.model.generated.ts
+++
@@ -1,5 +0,0 @@
-
-export interface Greeting {
-    content: string;
-    time: Date;
-}
--- gps-track-service-frontend/src-generated/index.ts
+++
@@ -1,4 +0,0 @@
-export { Greeting } from './greeting.model.generated';
-export { GreeterController } from './greetercontroller.generated';
-// export { APIModule } from './api.module';
-
